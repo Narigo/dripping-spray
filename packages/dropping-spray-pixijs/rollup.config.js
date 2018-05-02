d339f51402fcf73c54e5f701b2a57b262c397fae
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-05-02 15:54:52 +0200

    Clean up rollup configs
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- packages/dropping-spray-pixijs/rollup.config.js
+++ packages/dropping-spray-pixijs/rollup.config.js
@@ -1,4 +1,3 @@
-// rollup.config.js
 const commonjs = require("rollup-plugin-commonjs");
 const nodeResolve = require("rollup-plugin-node-resolve");
 
@@ -14,29 +13,6 @@ module.exports = {
       jsnext: true,
       main: true
     }),
-
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      //   include: 'node_modules/**',  // Default: undefined
-      //   exclude: ["node_modules/foo/**", "node_modules/bar/**"], // Default: undefined
-      // these values can also be regular expressions
-      // include: /node_modules/
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-      //   extensions: [".js", ".coffee"], // Default: [ '.js' ]
-      // if true then uses of `global` won't be dealt with by this plugin
-      //   ignoreGlobal: false, // Default: false
-      // if false then skip sourceMap generation for CommonJS modules
-      //   sourceMap: false, // Default: true
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-      //   namedExports: { "./module.js": ["foo", "bar"] }, // Default: undefined
-      // sometimes you have to leave require statements
-      // unconverted. Pass an array containing the IDs
-      // or a `id => boolean` function. Only use this
-      // option if you know what you're doing!
-      //   ignore: ["conditional-runtime-dependency"]
-    })
+    commonjs({})
   ]
 };
